4071d298aacd10577782aa7d4aab896a452a68e1
Yann Schoeni <y-schoeni@hotmail.com>
2022-03-18 11:04:32 +0100

    Add Link to handle react routing

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,17 @@
 import logo from "../public/medias/logo.svg";
-import "../styles/navbar.css";
+import "../styles/Navbar.css";
+import { Link } from "react-router-dom";
 
-export const Navbar = ({ title }: { title: string }) => {
-	return (
-		<div className="navbar">
-			<img src={logo} className="logo" alt="logo" />
-			<h2 className="title">{title}</h2>
-		</div>
-	);
+export const Navbar = ({ title }: any) => {
+  return (
+    <div className="navbar">
+      <Link to="/">
+        <img src={logo} className="logo" alt="logo" />
+      </Link>
+      <h2 className="title">{title}</h2>
+      <Link className="route-link" to="person">
+        New
+      </Link>
+    </div>
+  );
 };
